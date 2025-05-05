5b4ffdc65c326c1a87721c98e29a039b48939ffe
kaderiem006 <kaderiem007@gmail.com>
2025-05-05 18:36:54 +0600

    product page added

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/product', (req, res) => {
+    res.sendFile(path.join(__dirname, 'product.html'));
+});
+// Handle 404 for undefined routes
+app.use((req, res) => {
+    res.status(404).send('Page Not Found');
+});
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
